aa0c3d3e941950999f43356a0730e63ae7859712
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-07-09 13:39:42 -0400

    npub pro

---
+++ sw.js
@@ -0,0 +1,4 @@
+
+    importScripts("https://cdn.npubpro.com/index.js");
+    self.nostrSite.startSW([{ url: "https://cdn.npubpro.com/index.js", revision: "1720544364207" }, { url: "/sw.js", revision: "1720544364208" }, { url: "/manifest.webmanifest", revision: "1720544364209" }]);
+  
