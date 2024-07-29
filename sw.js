165f8b41bcf24143d3cb3354b3928a21b482c542
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-07-29 13:25:55 -0400

    bump js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 
     importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW([{ url: "https://cdn.npubpro.com/index.js", revision: "1720544364207" }, { url: "/sw.js", revision: "1720544364208" }, { url: "/manifest.webmanifest", revision: "1720544364209" }]);
+    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1722271716269" }, { url: "/sw.js", revision: "1722271716270" }, { url: "/manifest.webmanifest", revision: "1722271716271" }] });
   
