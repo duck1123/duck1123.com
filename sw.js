4add642bf464926ded987b7bd7b72eaf1ee02b7b
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-09-29 18:59:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,2 @@
-
-    importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1722271716269" }, { url: "/sw.js", revision: "1722271716270" }, { url: "/manifest.webmanifest", revision: "1722271716271" }] });
-  
+importScripts("https://cdn.npubpro.com/index.js");
+self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1758193331455" }, { url: "/sw.js", revision: "1758193331456" }, { url: "/manifest.webmanifest", revision: "1758193331457" }] });
